adaca456f4b45bbb3d90e9e6b4afb917435c86ab
Mohamed Ali <newmoeali@gmail.com>
2023-02-17 11:34:57 +0000

    added navbar and hero component with dynamic video  and text

--- src/components/hero/Hero.tsx
+++ src/components/hero/Hero.tsx
@@ -1,8 +1,25 @@
-import React from 'react'
+import React from 'react';
+import styles from "../hero/hero.module.css";
+import Image from 'next/image';
+
 
 function Hero() {
-  return (
-    <div>hero</div>
+  return ( 
+    <main className={styles.main} >
+      <div className={styles.text} >
+        <h3>
+          TV Show and Web Series Database
+        </h3>
+        <p>Create personalised schedules, episodes guides, cast crew and character information</p>
+      </div>
+      <div className={styles.videoContainer} >
+        <video autoPlay muted loop className={styles.videoBackground}>
+          <source src="/assets/trailer.mp4" type="video/mp4" />
+        </video>
+      </div>
+      <div className={styles.overlay} ></div>
+      
+    </main>
   )
 }
 
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,8 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from '@next/font/google'
 import styles from '@/styles/Home.module.css'
-import Hero from "../components/hero/hero"
+import Hero from "../components/hero/Hero";
+import NavBar from "../components/navbar/NavBar";
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -16,6 +17,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={styles.main}>
+        <NavBar/>
         <Hero/>
         
       </main>
