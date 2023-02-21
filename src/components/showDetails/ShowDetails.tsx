393cc6361f2087e261d5e4f358b921154960de09
Mohamed Ali <newmoeali@gmail.com>
2023-02-21 17:20:33 +0000

    fixed import styles typo in showDetails component

--- src/components/showDetails/ShowDetails.tsx
+++ src/components/showDetails/ShowDetails.tsx
@@ -1,6 +1,6 @@
 import { useQuery, gql } from "@apollo/client";
 import { useRouter } from "next/router";
-import styles from "../showDetails/showDetails.module.css";
+import styles from "../showDetails/showdetails.module.css";
 import Image from "next/image";
 import starFull from "../../../public/assets/star-full.png";
 import starEmpty from "../../../public/assets/star-empty.png";
@@ -21,7 +21,6 @@ const SHOW_QUERY = gql`
         name
         characterName
         characterImage
-
       }
     }
   }
@@ -84,68 +83,84 @@ function ShowDetails() {
             height={421.3}
           />
         </div>
-        <div className={styles.column} >
-         <div className={styles.stars}>
-          {stars.length > 0 && (
-            <div className={styles.starContainer}>{stars}</div>
-          )}
-          <p>Rating: {show.rating || "N/A"}</p>
-         </div>
-         <h1 className={styles.title}>{show.name}</h1>
+        <div className={styles.column}>
+          <div className={styles.stars}>
+            {stars.length > 0 && (
+              <div className={styles.starContainer}>{stars}</div>
+            )}
+            <p>Rating: {show.rating || "N/A"}</p>
+          </div>
+          <h1 className={styles.title}>{show.name}</h1>
           <div>
             <div
               className={styles.summary}
-              dangerouslySetInnerHTML={{ __html: show.summary }}>
-            </div>
+              dangerouslySetInnerHTML={{ __html: show.summary }}
+            ></div>
           </div>
         </div>
-       
       </section>
-      <section className={styles.bottomHalf} >
-          <div className={styles.showDetails}>
-                  <h2>Show Info</h2>
-                  <section className={styles.infoCards} >
-                    <div className={styles.card}>
-                        <p  id={styles["dark"]}>Streamed on</p>
-                        <p>{show.network || "N/A"}</p>
-                    </div>
-                    <div className={styles.card}>
-                        <p id={styles["dark"]}>Schedule</p>
-                        <p>{show.airDay || "N/A"}</p>
-                    </div>
-                    <div className={styles.card}>
-                        <p id={styles["dark"]}>Status</p>
-                        <p>{show.status || "N/A"}</p>
-                    </div>
-                    <div className={styles.card}>
-                        <p id={styles["dark"]}>Genres</p>
-                        <p>{show.genres ? show.genres[0]: "N/A"}</p>
-                    </div>
-                  </section >
+      <section className={styles.bottomHalf}>
+        <div className={styles.showDetails}>
+          <h2>Show Info</h2>
+          <section className={styles.infoCards}>
+            <div className={styles.card}>
+              <p id={styles["dark"]}>Streamed on</p>
+              <p>{show.network || "N/A"}</p>
+            </div>
+            <div className={styles.card}>
+              <p id={styles["dark"]}>Schedule</p>
+              <p>{show.airDay || "N/A"}</p>
+            </div>
+            <div className={styles.card}>
+              <p id={styles["dark"]}>Status</p>
+              <p>{show.status || "N/A"}</p>
+            </div>
+            <div className={styles.card}>
+              <p id={styles["dark"]}>Genres</p>
+              <p>{show.genres ? show.genres[0] : "N/A"}</p>
+            </div>
+          </section>
+        </div>
+        {show.cast.length > 0 && (
+          <section className={styles.cast}>
+            <h2>Starring</h2>
+
+            {show.cast.map(
+              (castMember: {
+                name: string;
+                characterName: string;
+                characterImage: string;
+              }) => (
+                <div className={styles.castCard} key={castMember.name}>
+                  <div className={styles.imageCropper}>
+                    <Image
+                      width={100}
+                      height={100}
+                      className={styles.castImage}
+                      src={castMember.characterImage}
+                      alt="castImage"
+                    />
+                  </div>
+                  <div className={styles.castName}>
+                    <p id={styles["dark"]}>{castMember.name}</p>
+                    <p>{castMember.characterName}</p>
+                  </div>
                 </div>
-                {show.cast.length > 0 && (
-                  <section className={styles.cast}>
-                    <h2>Starring</h2>
-                  
-                      {show.cast.map((castMember: { name: string, characterName: string, characterImage: string }) => (
-                        <div className={styles.castCard} key={castMember.name}>
-                          <div className={styles.imageCropper}><Image width={100}
-          height={100} className={styles.castImage} src={castMember.characterImage} alt="castImage" /></div>
-                          <div className={styles.castName} >
-                           <p id={styles["dark"]}>{castMember.name}</p>
-                           <p>{castMember.characterName}</p>
-                          </div>
-                        </div>
-                      ))}
-                    
-                  </section>
-                )}
+              )
+            )}
+          </section>
+        )}
       </section>
-      
-       <footer className={styles.footer}>
-      <p>TV Bland - your one-stop destination for all your favorite TV shows! From drama to comedy, we&apos;ve got you covered. Don&apos;t miss out on our comprehensive TV schedule and never miss a show again. Stay up to date with the latest news and reviews of your favorite programs. TV Bland &copy; 2023
-</p>
-    </footer>
+
+      <footer className={styles.footer}>
+        <p>
+          TV Bland - your one-stop destination for all your favorite TV shows!
+          From drama to comedy, we&apos;ve got you covered. Don&apos;t miss out
+          on our comprehensive TV schedule and never miss a show again. Stay up
+          to date with the latest news and reviews of your favorite programs. TV
+          Bland &copy; 2023
+        </p>
+      </footer>
     </div>
   );
 }
