a71ae6bb9d471ce5f0aa3353838769e65b34b0d8
Mohamed Ali <newmoeali@gmail.com>
2023-02-21 15:57:37 +0000

    added navbar logo link

--- src/components/navbar/NavBar.tsx
+++ src/components/navbar/NavBar.tsx
@@ -2,12 +2,15 @@ import React from 'react'
 import styles from "../navbar/navbar.module.css";
 import searchIcon from "../../../public/assets/search.png"
 import Image from 'next/image'
+import Link from 'next/link';
  
 function NavBar() {
   return (
     <nav className={styles.navbar}>
       <section className={styles.navitems}>
-          <h2 id='logo'>TV BLAND</h2>
+        <Link className={styles.logoLink} href="/">
+          <h2 >TV BLAND</h2>
+        </Link>
         <Image
             id='icon'
             src={searchIcon}
